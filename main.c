e08dcbe8d4bf52e7f172817aebc1e3154521d682
Nakzz <Naqibprio10@gmail.com>
2020-10-10 23:28:47 +0000

    implemented reader with getline

--- main.c
+++ main.c
@@ -2,35 +2,82 @@
 
 int main()
 {
-
     reader();
 }
+
+void copyArray(char *line, char buffer){
+            printf("%s", line);
+
+}
+
 
 char ** reader()
 {
 
     char ch;
-    char buffer[50960];
- 
+    char buffer= malloc(sizeof(char) * 50960);
+    // char line;
+    int lineSize;
 
-    do{
-        ch=getc(stdin);
+  char *line = NULL;
+  size_t len = 0;
+  ssize_t read;
 
+ while ((read = getline(&line, &len, stdin)) != -1) {
+     if(read > 50960)
+      fprintf( stderr, "Error: Line too long\n");
+      
+    printf("Retrieved line of length %zu :\n", read);
+    // printf("%s", line);
+
+    copyArray(line, buffer);
+
+
+  }
+
+    // do{
+    //     if(lineSize>=50960){
+    //         fprintf( stderr, "Error: Line too long\n");
+    //         //TODO: flush until \n is found
+    //     }
+
+    //     ch=getc(stdin);
+    //     // strcat(buffer,ch);
+    //     // buffer[&buffer+ lineSize] = ch;
+
+    //     append(buffer, ch, lineSize);
+
+    //     lineSize++;
         
-        if(ch == '\n'){
-            //add to array
-            printf("%s", buffer);
+    //     if(ch == '\n'){
+    //         printf("%s", buffer);
 
-        } else{
-        strcat(buffer,ch);
+    //         line = malloc(sizeof(char)*lineSize);
+    //         if(line == NULL){
+    //             printf("%s\n", "Error: Bad Malloc");
+	// 		    exit(-1);
+    //         }
+    //         // strcpy(line, buffer);
+    //         copyArray(line, buffer, lineSize);
 
-        // printf("%c", ch);
+    //         //pass the string to the queue
+    //         // EnqueueString(q1, buffer);
 
-        }
+    //         lineSize=0;
+    //         // buffer = NULL;
+            
+    //     } else{
+            
+        
+    //     // printf("%c", ch);
 
-    }while(ch != EOF);
+    //     }
+
+    // }while(ch != EOF);
 
     //remaining buffer should be added
+    // free(buffer);
+    // free(ch);
 
     return NULL;
 
@@ -38,3 +85,4 @@ char ** reader()
 
 
 }
+
